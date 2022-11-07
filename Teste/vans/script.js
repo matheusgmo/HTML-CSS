a0549699c457db9cfff55a785fbf988c41ff1b53
Matheus Moraes <107554927+matheusgmo@users.noreply.github.com>
2022-11-07 12:58:50 -0300

    vans_att1

---
+++ Teste/vans/script.js
@@ -0,0 +1,17 @@
+function barra(){
+    if(search.style.display == 'flex'){
+        search.style.display = 'none'
+    }
+    else{
+        search.style.display = 'flex'
+    }
+}
+
+function loc(){
+    if(location.style.display == 'flex'){
+        location.style.display = 'none'
+    }
+    else{
+        location.style.display = 'flex'
+    }
+}
